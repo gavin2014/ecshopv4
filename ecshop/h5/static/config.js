48d9199309b6a70ee6f77b9f2f163299c63a68c3
王世铭 <812568591@qq.com>
2019-05-17 14:43:21 +0800

    first commit

---
+++ ecshop/h5/static/config.js
@@ -0,0 +1,10 @@
+const env = {  
+  'API_HOST': 'http://api.ecshop4.lywang.win',
+  // 'API_HOST': 'http://api.ecshop4.lywang.win',
+  'APP_NAME': 'Demo',
+  'APP_DESC': 'This is a  demo',
+  'APP_KEYWORDS': 'Demo, Shop',
+  'DEBUG': false,
+  'ENCRYPTED': false,
+  'VERSION': '1.0.0', // API版本号
+}
